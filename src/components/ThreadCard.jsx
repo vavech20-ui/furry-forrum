6593dd35300322aed976d6e356196da427805d55
vavech20-ui <vavech20@gmail.com>
2026-05-24 20:15:45 +0300

    Add files via upload

--- src/components/ThreadCard.jsx
+++ src/components/ThreadCard.jsx
@@ -1,9 +1,14 @@
+import CommentSection from './CommentSection';
+
 const ThreadCard = ({ thread }) => {
   return (
     <div className="thread-card">
       <h3>{thread.title}</h3>
       <p>{thread.body}</p>
       <small>Автор: {thread.author}</small>
+
+      {/* Комментарии к треду */}
+      <CommentSection threadId={thread.id} />
     </div>
   );
 };
